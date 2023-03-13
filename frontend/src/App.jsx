418f8ae00d053e77990b31def3aeecb98e304b58
Ismail-Lh <ismailsaad03@gmail.com>
2023-03-13 16:08:42 +0100

    Add root routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Register from './components/Register';
+import Username from './components/Username';
+import Password from './components/Password';
+import PageNotFound from './components/PageNotFound';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+
+// ?: Root routes
+const router = createBrowserRouter([
+	{ path: '/', element: <Username /> },
+	{ path: '/register', element: <Register /> },
+	{ path: '/password', element: <Password /> },
+	{ path: '/profile', element: <Profile /> },
+	{ path: '/reset', element: <Reset /> },
+	{ path: '/recovery', element: <Recovery /> },
+	{ path: '*', element: <PageNotFound /> },
+]);
+
 function App() {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+	return (
+		<main>
+			<RouterProvider router={router} />
+		</main>
+	);
 }
 
 export default App;
