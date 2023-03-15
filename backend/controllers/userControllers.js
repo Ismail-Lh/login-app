d50436551a9a9a42eaf0eb12949339336367493d
Ismail-Lh <ismailsaad03@gmail.com>
2023-03-15 17:17:38 +0100

    Create the getUser by name backend functionality

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,8 +1,22 @@
+import User from '../models/User.js';
+
 // *@desc Get a user by name
 // *@route GET /api/users/:username
 // *@access PRIVATE
 export const getUser = async (req, res) => {
-	res.json('getUser route');
+	const { username } = req.params;
+
+	const user = await User.findOne({ username })
+		.select('-password')
+		.lean()
+		.exec();
+
+	if (!user)
+		res.status(404).json({
+			message: 'No user found with this name. Please enter another username.',
+		});
+
+	res.status(201).json({ user });
 };
 
 // *@desc Update the user profile
