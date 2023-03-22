fa9fd5948e2476007e2dccb6e2012beef0855338
Ismail-Lh <ismailsaad03@gmail.com>
2023-03-22 19:18:36 +0000

    Password recovery frontend part-1

--- frontend/src/components/Recovery.jsx
+++ frontend/src/components/Recovery.jsx
@@ -1,8 +1,51 @@
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { toast, Toaster } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { generateOtp, verifyOtp } from '../lib/apiRequest';
+import { useAuthStore } from '../store';
 import styles from '../styles/Username.module.css';
 
 const Recovery = () => {
+	const { username } = useAuthStore(state => state.auth);
+
+	const navigate = useNavigate();
+
+	const [code, setCode] = useState();
+
+	const { data: otpCode } = useQuery({
+		queryKey: ['OTP'],
+		queryFn: () => generateOtp(username),
+		onSuccess: () => {
+			toast.success(<b>OTP has been send to your email address.</b>);
+		},
+		onError: error => {
+			toast.error(<b>{error?.response.data.message}</b>);
+		},
+	});
+
+	const verifyOtpQuery = useQuery({
+		queryKey: ['OTP'],
+		queryFn: verifyOtp,
+		onSuccess: () => {
+			toast.success(<b>OTP verify successfully!</b>);
+			navigate('/reset');
+		},
+		onError: error => {
+			toast.error(<b>{error?.response.data.message}</b>);
+		},
+	});
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		verifyOtpQuery.query({ username, otpCode: code });
+	};
+
 	return (
 		<div className='container mx-auto'>
+			<Toaster position='top-center' reverseOrder={false} />
+
 			<div className='flex justify-center items-center h-screen'>
 				<div className={styles.glass}>
 					<div className='title flex flex-col items-center'>
@@ -12,7 +55,7 @@ const Recovery = () => {
 						</span>
 					</div>
 
-					<form className='pt-20'>
+					<form className='pt-20' onSubmit={handleSubmit}>
 						<div className='textbox flex flex-col items-center gap-6'>
 							<div className='input text-center'>
 								<span className='py-4 text-sm text-left text-gray-500'>
@@ -22,6 +65,7 @@ const Recovery = () => {
 									className={styles.textbox}
 									type='text'
 									placeholder='Enter your OTP'
+									onChange={e => setCode(e.target.value)}
 								/>
 							</div>
 							<button type='submit' className={styles.btn}>
